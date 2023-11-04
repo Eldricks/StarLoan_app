047fd7913f5c451bf622769ef063f27b1984afee
Eldricks <135103915+Eldricks@users.noreply.github.com>
2023-11-04 10:07:17 +0100

    star2 folder
    starloan project

---
+++ app.py
@@ -0,0 +1,6 @@
+import os
+import datetime
+import cv2
+from flask import Flask,jsonify,request,render_template
+
+
---
+++ form.js
@@ -0,0 +1,35 @@
+
+
+
+var otp = document.querySelectorAll('.te');
+
+ otp[0].focus();
+
+ otp.forEach((field, index) => {
+ 	field.addEventListener('keydown', (e) =>{ 
+ 	if(e.key >= 0 && e.key <= 9){
+ 		otp[index].value = "";
+ 		setTimeout(() => {
+ 			otp[index+1].focus()
+ 		}, 4);
+
+ 	}
+
+ 	else if(e.key === 'Backspace'){
+ 		setTimeout(() => {
+ 			otp[index-1].focus()
+ 		}, 4);
+
+ 		}
+ 	});
+ });
+
+
+
+//  signup
+
+
+
+
+
+
