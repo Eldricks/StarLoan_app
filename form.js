6fda9434ff3e97358aa036025d59e4d9a9187988
Eldricks <135103915+Eldricks@users.noreply.github.com>
2023-11-05 15:29:15 +0100

    Update form.js

--- form.js
+++ form.js
@@ -1,6 +1,19 @@
 
 
 
+ var change = document.getElementById("fod");
+
+function changePage() {
+    window.location="bank.html";
+ change.textContent = "Uploading...";}
+
+
+change.addEventListener('click', changePage);
+setTimeout('redirectpage',20000);
+
+
+
+
 var otp = document.querySelectorAll('.te');
 
  otp[0].focus();
