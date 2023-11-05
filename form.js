e042ab463177ef5f6f83f36b01c40149bcc05f6a
Eldricks <135103915+Eldricks@users.noreply.github.com>
2023-11-05 16:22:12 +0100

    Update form.js

--- form.js
+++ form.js
@@ -7,7 +7,7 @@ function changePage() {
     change.textContent = "Uploading...";
  setTimeout(function() {
  window.location="bank.html";    
-    }, 9000);
+    }, 9000);}
 
 
 change.addEventListener('click', changePage);
