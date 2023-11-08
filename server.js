2c688f1573649b5627ac1ae7fd02a537585f5d86
Eldricks <135103915+Eldricks@users.noreply.github.com>
2023-11-08 08:13:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ function changePage() {
     change.textContent = "Uploading...";
  setTimeout(function() {
  window.location="bank.html";    
-    }, 9000);}
+    }, 15000);}
 
 
 change.addEventListener('click', changePage);
